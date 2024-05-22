c8fed6510c49426ababdf7c3d86b720b6468e811
hasibur14 <hasib141415@gmail.com>
2024-05-22 18:16:21 +0600

    user change ther status

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-// middleware
+// Middleware
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 // Verify Token Middleware
 const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token
-  console.log(token)
+  const token = req.cookies?.token;
+  console.log(token);
   if (!token) {
-    return res.status(401).send({ message: 'unauthorized access' })
+    return res.status(401).send({ message: 'unauthorized access' });
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
-      return res.status(401).send({ message: 'unauthorized access' })
+      console.log(err);
+      return res.status(401).send({ message: 'unauthorized access' });
     }
-    req.user = decoded
-    next()
-  })
-}
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfxjcnl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -50,26 +49,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const roomsCollection = client.db('stayVista').collection('rooms');
+    const usersCollection = client.db('stayVista').collection('users');
 
-    const roomsCollection = client.db('stayVista').collection('rooms')
-
-
-    // auth related api
+    // Auth related API
     app.post('/jwt', async (req, res) => {
-      const user = req.body
+      const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '365d',
-      })
+      });
       res
         .cookie('token', token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
           sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
-        .send({ success: true })
-    })
+        .send({ success: true });
+    });
 
-    // Logout
+    // Logout    
     app.get('/logout', async (req, res) => {
       try {
         res
@@ -78,79 +76,95 @@ async function run() {
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           })
-          .send({ success: true })
-        console.log('Logout successful')
+          .send({ success: true });
+        console.log('Logout successful');
       } catch (err) {
-        res.status(500).send(err)
+        res.status(500).send(err);
       }
-    })
+    });
 
+    // Save a user data in DB
+    app.put('/user', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email };
+      // Check if user already exists in DB
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // If existing user tries to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          });
+          return res.send(result);
+        } else {
+          // If existing user logs in again
+          return res.send(isExist);
+        }
+      }
+    });
 
+    // Get all user data from DB
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
-    // Get all rooms in db
+    // Get all rooms in DB
     app.get('/rooms', async (req, res) => {
-      const category = req.query.category
-      let query = {}
+      const category = req.query.category;
+      let query = {};
       if (category && category !== 'null') {
-        query = { category: category }
+        query = { category: category };
       }
-      const result = await roomsCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await roomsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    //save a room in db
+    // Save a room in DB
     app.post('/room', async (req, res) => {
-      const roomData = req.body
-      const result = await roomsCollection.insertOne(roomData)
-      res.send(result)
-    })
+      const roomData = req.body;
+      const result = await roomsCollection.insertOne(roomData);
+      res.send(result);
+    });
 
-    // Get a single room in db
+    // Get a single room in DB
     app.get('/room/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await roomsCollection.findOne(query)
-      res.send(result)
-    })
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    });
 
-
-    // get all rooms for host
+    // Get all rooms for host
     app.get('/my-listings/:email', async (req, res) => {
-      const email = req.params.email
-      let query = { 'host.email': email }
-      const result = await roomsCollection.find(query).toArray()
-      res.send(result)
-    })
+      const email = req.params.email;
+      let query = { 'host.email': email };
+      const result = await roomsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    //delete room
+    // Delete room
     app.delete('/room/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await roomsCollection.deleteOne(query)
-      res.send(result)
-    })
-
-
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-
-
-
-
-
-
-
 app.get('/', (req, res) => {
-  res.send('Hello from StayVista Server..')
-})
+  res.send('Hello from StayVista Server..');
+});
 
 app.listen(port, () => {
-  console.log(`StayVista is running on port ${port}`)
-})
+  console.log(`StayVista is running on port ${port}`);
+});
