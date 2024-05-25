455e7aff4276cf6ee93dbf02fc23ac18c4d6fb66
hasibur14 <hasib141415@gmail.com>
2024-05-25 20:38:35 +0600

    update

--- index.js
+++ index.js
@@ -133,6 +133,19 @@ async function run() {
       res.send(result);
     });
 
+    //update a user role
+    app.patch('/users/update/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email }
+      const updateDoc = {
+        $set: { ...user, timestamp: Date.now() },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+
 
     // Get all rooms in DB
     app.get('/rooms', async (req, res) => {
